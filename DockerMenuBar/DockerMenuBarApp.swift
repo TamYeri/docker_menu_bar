b124ad4adbd4dbcde00c439cad11415a18ee1b2a
Mesut KURT <mkurt@mesutkurt.net>
2025-09-14 13:49:40 +0300

    Initial Commit

---
+++ DockerMenuBar/DockerMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockerMenuBarApp.swift
+//  DockerMenuBar
+//
+//  Created by Mesut KURT on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct DockerMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DockerMenuBarTests/DockerMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockerMenuBarTests.swift
+//  DockerMenuBarTests
+//
+//  Created by Mesut KURT on 14.09.2025.
+//
+
+import Testing
+@testable import DockerMenuBar
+
+struct DockerMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
